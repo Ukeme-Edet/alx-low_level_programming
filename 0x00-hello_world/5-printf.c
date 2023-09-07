0f93d8eb7dddfcd32740d45fb0fa30ba2a66f123
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-07 11:17:08 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - The program entry point
+ * Return: 0 if program is succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
