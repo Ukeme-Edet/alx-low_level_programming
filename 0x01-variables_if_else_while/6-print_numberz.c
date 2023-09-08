64e4925cd8c53dc97be941ff96f82d9436d8ce65
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-08 17:07:22 +0100

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The program entry point
+ * Return: 0 if the program is successful
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 59)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
