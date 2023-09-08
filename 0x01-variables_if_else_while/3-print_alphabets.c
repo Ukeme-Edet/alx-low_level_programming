2843376e652424b4cbc8d64e73c6306c907c3410
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-08 16:49:16 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-	for (int i = 97; i < 122; i++)
+	int i;
+
+	for (i = 97; i < 122; i++)
 		putchar(i);
-	for (int i = 65; i < 90; i++)
+	for (i = 65; i < 90; i++)
 		putchar(i);
 	putchar(10);
 	return (0);
