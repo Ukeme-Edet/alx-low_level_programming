4b443be2545f33fd1053a96f5d56b365121d419d
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-08 17:26:12 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The program entry point
+ * Return: 0 if the program is successful
+ */
+int main(void)
+{
+	int i = 48;
+	while (i < 58)
+	{
+		putchar(i++);
+		if (i == 58)
+			continue;
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
