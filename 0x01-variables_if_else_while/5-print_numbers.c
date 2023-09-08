2ff2a231089f33906ec4ed28a8e8ecfb2c64732d
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-08 17:02:54 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The program entry point
+ * Return: 0 if the program is successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+		printf("%d", i++);
+	printf("\n");
+	return (0);
+}
