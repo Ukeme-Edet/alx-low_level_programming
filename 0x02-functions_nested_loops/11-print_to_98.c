716cf0600d06970b47fe93b26a8054406c929f21
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-12 19:54:43 +0100

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,11 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		printf("%d", n);
+		printf("%d, ", n);
 		if (n < 98)
 			n++;
 		else
 			n--;
-		_putchar(',');
-		_putchar(' ');
 	}
 	_putchar('9');
 	_putchar('8');
