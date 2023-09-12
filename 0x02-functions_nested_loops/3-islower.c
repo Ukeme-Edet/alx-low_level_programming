e95f2c4c650245de402bbf96ce2f48f6249c8bde
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-12 17:14:07 +0100

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,10 @@
 
 /**
  * _islower - Checks if a character is a lowercase letter
+ * @c: The character to checked
  * Return: 1 if the character is a lower case letter else 1
  */
-int _islower(int c) {
-	return !(c > 'z' || c < 'a');
+int _islower(int c)
+{
+	return (!(c > 'z' || c < 'a'));
 }
