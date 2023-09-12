5e832339f5fe60839bc93ddde47e5c44b42ddc7c
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-12 16:30:16 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,2 @@
 #include <stdio.h>
-
-/**
- * main - The program entry point
- * Return: 0 of the program is successful
- */
-int main(void)
-{
-	printf("__putchar\n");
-	return (0);
-}
+#include "main.h"
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - The program entry point
+ * Return: 0 if the program is successful
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
