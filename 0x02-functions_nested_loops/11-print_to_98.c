7e5ecb3efa0d7185dbfb57f546fab39ad28870af
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-12 19:07:31 +0100

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all integer numbers from n to 98
+ * @n: The integer to start with
+ */
+void print_to_98(int n)
+{
+	for (; n < 99; n++)
+	{
+		if (n > 10)
+			_putchar(n / 10 + '0');
+		putchar(n % 10 + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n);
+}
