ef266cde949dd8bc6141ed54238e56b378586c3a
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-09-18 19:18:53 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** _strcpy - Copies a string into a buffer :)
+ * @dest: The buffer to be copied into
+ * @src: The string to be copied
+ * Return: A pointer to the buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+		dest[i] = src[i++];
+	dest[++i] = '\0';
+	return (dest);
+}
