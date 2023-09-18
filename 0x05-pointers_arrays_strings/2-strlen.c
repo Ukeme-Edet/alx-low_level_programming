94fc87516363b4c574c94d5cb03414617ba9ed94
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-09-18 17:03:18 +0100

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - Gets the length of a string
+ * @s: Pointer to the string
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s[i])
+		i++
+	return (i);
+}
