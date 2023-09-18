ef6c5257a072b4ea12ace435430c8956527b863b
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-09-18 18:46:13 +0100

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of the string
+ * @str: The string to be printed
+ */
+void puts_half(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[i])
+		i++;
+	len = i;
+	for (i = len - (len / 2); i < len; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ int _strlen(char *s);
 void _puts(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *s);
+void puts2(char *str);
+void puts_half(char *str);
 
 #endif /* MAIN_H */
