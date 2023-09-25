9369460b7aa3060353754753143533eb499c4db4
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-09-25 12:56:12 +0100

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: A pointer to the string to be searched
+ * @accept: A pointer to the string of bytes acceptable
+ * Return: The lenght of substring :)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, alen = 0;
+
+	while(accept[alen])
+		alen++;
+	while (s[i])
+	{
+		for (j = 0; j < alen; j++)
+		{
+			if (a[i] == accept[j])
+			{
+				i++;
+				break;
+			}
+		}
+		if (j == alen)
+			return (i);
+	}
+	return (i);
+}
