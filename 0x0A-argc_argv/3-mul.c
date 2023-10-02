07f5903c5b823a124c08fb8be7b09a3b5080facb
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-02 15:13:58 +0100

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.c"
+#include <stdio.h>
+#include <Stdlib.h>
+
+/**
+ * main - The program's entry point :)
+ * @argc: The number of command line arguments passed
+ * @argv: An array of pointers to the strings of arguments passed
+ * Return: 0 if the program is successful, else 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
