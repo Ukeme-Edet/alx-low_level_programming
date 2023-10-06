a4215a4f4bad16903d8b06854c3c5ee5f1a40abb
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-06 09:48:02 +0100

    Fixed Betty Issue :)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,6 +5,7 @@
  * string_nconcat - Concatenates two strings
  * @s1: The first string to be concatenated
  * @s2: The second string to be concatenated
+ * @n: The max amount of bytes from s2 to be concatenated
  * Return: A pointer to the concatenated string if successful, else NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -12,11 +13,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, s1l, s2l;
 	char *p;
 
-	while (s1[i]) i++;
+	while (s1[i])
+		i++;
 	s1l = i;
-	while (s2[i - s1l]) i++;
+	while (s2[i - s1l])
+		i++;
 	s2l = i - s1l;
-	if (s2l < n) n = s2l;
+	if (s2l < n)
+		n = s2l;
 	p = malloc((s1l + n + 1) * sizeof(char));
 	i = 0;
 	while (p && s1[i])
