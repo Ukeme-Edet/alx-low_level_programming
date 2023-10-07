d5e580d2c372b384c9d239ab9d4836ed7d319ef0
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-07 20:12:35 +0100

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,28 +2,6 @@
 #include <stdlib.h>
 
 /**
- * main - A program that multiplies two numbers together
- * @argc: The number of arguments passed to the program
- * @argv: An array of the arguments passed
- * Return: 0 if the program is successful, else 1
- */
-int main(int argc, char **argv)
-{
-	char *num1, *num2, *product;
-
-	num1 = argv[1];
-	num2 = argv[2];
-	if (!is_digit(num1) || !is_digit(num2) || argc != 3)
-		error();
-	product = mul(num1, num2);
-	if (!product)
-		error();
-	print_s(product);
-	free(product);
-	return (0);
-}
-
-/**
  * error - Prints out an error message and exits the program
  */
 void error(void)
@@ -64,48 +42,54 @@ void print_s(char *s)
 }
 
 /**
- * mul - Multiplies two numbers together
- * @num1: The first number to be multiplied
- * @num2: The second number to be multiplied
- * Return: A pointer to the product of the two numbers
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
-char *mul(char *num1, char *num2)
+int main(int argc, char *argv[])
 {
-	int len1, len2, i, j, k, carry, n1, n2, sum;
-	char *product;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	product = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (!product)
-		return (NULL);
-	for (i = 0; i < len1 + len2; i++)
-		product[i] = '0';
-	for (i = len1 - 1; i > -1; i--)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		error();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		n1 = num1[i] - '0';
-		for (j = len2 - 1; j > -1; j--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			n2 = num2[j] - '0';
-			sum = (n1 * n2) + carry;
-			carry = sum / 10;
-			product[i + j + 1] += (sum % 10);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (carry)
-			product[i + j + 1] += carry;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	for (k = 0; k < len1 + len2; k++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (product[k] != '0')
-			break;
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	if (k == len1 + len2)
-		k--;
-	for (i = 0; i < len1 + len2; i++, k++)
-		product[i] = product[k];
-	product[i] = '\0';
-	return (product);
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
 
 /**
