49072b62d04c5de54062f6fc7985a83a9656cdc9
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-19 13:45:08 +0100

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_first - Prints a string before the main function is executed
+ *
+ * Return: Always void
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		   "I bore my house upon my back!\n");
+}
