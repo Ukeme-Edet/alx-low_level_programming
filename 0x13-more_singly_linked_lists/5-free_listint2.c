a7e64bfdc85c29d0a419b38c93f95614fd30dfd7
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-23 21:17:40 +0100

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
-	while (*head)
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
 	{
 		next = (*head)->next;
 		free(*head);
 		*head = next;
 	}
+
+	head = NULL;
 }
