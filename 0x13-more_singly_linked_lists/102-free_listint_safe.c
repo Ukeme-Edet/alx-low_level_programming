71f154067ee968df50efefb1a28d00e41a7be010
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-24 16:37:42 +0100

    Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t list safely.
+ * @h: A pointer to the address of the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t nodes, index;
+
+	nodes = looped_listint_len(*h);
+
+	if (nodes == 0)
+	{
+		for (; *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
+}
