10f121ff1ceafb04d1755284c20ac98efe6ba78b
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-26 19:40:24 +0100

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int test = 1;
+	return (*((char *)&test));
+}
