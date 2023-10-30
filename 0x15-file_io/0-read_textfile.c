43ae860e25352df4e26a812bcb93a0be0b225c9e
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-30 12:06:02 +0100

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -10,25 +11,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
-	char *buffer;
-	size_t read_count, write_count;
+	int fd, len = 0;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-	file = fopen(filename, "r");
-	if (file == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	read_count = fread(buffer, sizeof(char), letters, file);
-	if (read_count == 0)
-		return (0);
-	write_count = fwrite(buffer, sizeof(char), read_count, stdout);
-	if (write_count == 0)
-		return (0);
-	fclose(file);
-	free(buffer);
-	return (write_count);
+	len = read(fd, buf, letters);
+	write(STDOUT_FILENO, buf, len);
+	close(fd);
+	free(buf);
+	return (len);
 }
