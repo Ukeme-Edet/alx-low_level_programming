9aa60aa9bdbfb5c2210d302b5a1911c264ae6778
Ukeme Edet <92282594+Ukeme-Edet@users.noreply.github.com>
2023-10-30 11:53:07 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 /**
  * create_file - A function that creates a file
@@ -10,16 +11,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *new_file;
-	int written_bytes;
+	int fd, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	new_file = fopen(filename, "w");
-	written_bytes = fwrite(text_content, sizeof(char), strlen(text_content),
-						   new_file);
-	if (written_bytes == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-	fclose(new_file);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		write(fd, text_content, len);
+	}
+	close(fd);
 	return (1);
 }
