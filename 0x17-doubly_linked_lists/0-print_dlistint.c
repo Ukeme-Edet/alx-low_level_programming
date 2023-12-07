c91183a9aa850365c86cc37a4625a8775e4efbb4
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-07 16:35:18 +0100

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
-#include "lists.h"
 #include <stddef.h>
+#include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
